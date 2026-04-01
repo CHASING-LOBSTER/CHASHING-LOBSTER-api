99f54a53e543722bc796a6f44b5e733ba4f9fe74
akshatkatwala <arkatwala501@gmail.com>
2026-04-01 11:12:50 -0400

    Added GetItems() method

--- Chasing-Lobster-api/Chasing.Lobster.Api/Controllers/CatalogController.cs
+++ Chasing-Lobster-api/Chasing.Lobster.Api/Controllers/CatalogController.cs
@@ -8,8 +8,13 @@ namespace Chasing.Lobster.Api.Controllers{
     public class CatalogController: ControllerBase{
         [HttpGet]
         public IActionResult GetItems(){
-            return Ok("Hello World!");
-        }
+            var items = new List<Item>(){
+            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+            new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+    };
+
+    return Ok(items);
+}
     }
     
 }
