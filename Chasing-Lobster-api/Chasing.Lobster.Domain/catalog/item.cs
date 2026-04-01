6c6d6ecfbc45b2c79c055cfc327722cd96f6852a
akshatkatwala <arkatwala501@gmail.com>
2026-04-01 11:06:50 -0400

    Added domain models and catalog controller

---
+++ Chasing-Lobster-api/Chasing.Lobster.Domain/catalog/item.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Chasing.Lobster.Domain.catalog{
+        public class Item{
+        public int Id {get; set;}
+        public string? Name {get; set;}
+        public string? Description {get; set;}
+        public string? Brand {get; set;}
+        public decimal Price {get; set;}
+        public List<Rating>? Ratings {get; set;}
+
+        public void AddRating(Rating rating){
+            this.Ratings.Add(rating);
+        }
+
+        public Item(string name, string description, string brand, decimal price){
+        if(string.IsNullOrEmpty(name)){
+            throw new ArgumentException(name);
+        }
+
+        if(string.IsNullOrEmpty(description)){
+            throw new ArgumentException(description);
+        }
+
+        if(string.IsNullOrEmpty(brand)){
+            throw new ArgumentException(brand);
+        }
+        
+        if(price < 0.00m){
+            throw new ArgumentException("Price must be greater than or equal to 0.");
+        }
+       
+        
+        Name = name;
+        Description = description;
+        Brand = brand; 
+        Price = price;
+        Ratings = new List<Rating>();
+    }   
+
+    }
+
+
+
+
+}
