25da21ac999ecf5df12c00e7a91f6cbfe05b8630
akshatkatwala <arkatwala501@gmail.com>
2026-04-12 21:10:47 -0400

    Add Rating unit tests

---
+++ tests/Chasing.Lobster.Domain.Tests/RatingTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Chasing.Lobster.Domain.Tests;
+using Chasing.Lobster.Domain.catalog;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public sealed class RatingTests
+{
+ [TestMethod]
+public void Can_Create_New_Rating()
+{
+    var rating = new Rating(1, "Mike", "Great fit!");
+
+    Assert.AreEqual(1, rating.Stars);
+    Assert.AreEqual("Mike", rating.UserName);
+    Assert.AreEqual("Great fit!", rating.Review);
+}
+
+[TestMethod]
+public void Cannot_Create_Rating_With_Invalid_Stars()
+{
+    try
+    {
+        var rating = new Rating(0, "Mike", "Great fit!");
+        Assert.Fail("Expected ArgumentException was not thrown.");
+    }
+    catch (ArgumentException)
+    {
+        // Test passes
+    }
+}
+}
